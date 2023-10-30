b55ca4c3960c9a363d3fad14b7119972a1b09ec6
tjdus9503 <tjdus03095@gmail.com>
2023-10-30 09:49:31 +0900

    Calc class

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,7 @@
+package com.ll;
+
+public class Calc {
+    public static int run(String expression) {
+        return 0;
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,8 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(
+                "결과 : " + (Calc.run("3 * 1") == 3)
+        );
     }
 }
