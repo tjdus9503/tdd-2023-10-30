19256cbb3f433021e0a788f0ec579a4b5fd6733b
tjdus9503 <tjdus03095@gmail.com>
2023-10-30 09:58:11 +0900

    step 1

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,6 +2,13 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        return 0;
+        String[] sArr = expression.split("\\*");
+        sArr[0] = sArr[0].trim();
+        sArr[1] = sArr[1].trim();
+
+        int left = Integer.parseInt(sArr[0]);
+        int right = Integer.parseInt(sArr[1]);
+
+        return left * right;
     }
 }
